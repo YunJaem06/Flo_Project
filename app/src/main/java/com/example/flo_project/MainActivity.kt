46a9c70d470cacc57fc3a3c9c7f84e293463927d
YunJaem06 <yunjae0616@gmail.com>
2022-06-28 22:42:42 +0900

    Flo 둘러보기 화면 구현중

--- app/src/main/java/com/example/flo_project/MainActivity.kt
+++ app/src/main/java/com/example/flo_project/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.flo_project.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        val bottomNavigationView = binding.navigationMain
 
         val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
         navController?.let {
