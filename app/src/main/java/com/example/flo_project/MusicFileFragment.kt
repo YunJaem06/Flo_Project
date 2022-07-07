f48487db8672b7fd5c2bf2509d7fed38a0f70629
YunJaem06 <yunjae0616@gmail.com>
2022-07-07 22:36:15 +0900

    Flo String 정리 완료

--- app/src/main/java/com/example/flo_project/MusicFileFragment.kt
+++ app/src/main/java/com/example/flo_project/MusicFileFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo_project.databinding.FragmentLockerMusicfileBinding
-import com.example.flo_project.databinding.FragmentLockerSavesongBinding
 
 class MusicFileFragment : Fragment() {
 
